21caba186e879ad7c754c71454fa857e631a94e6
pony <13500815917@163.com>
2020-08-19 15:18:00 +0800

    添加打包文件

---
+++ build/precache-manifest.6fda3ff62f1eaac3e92e928d6c6839ae.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fe786af38d7665d61f705c13d90f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1ea89b9dc5358086c3",
+    "url": "/static/css/main.0d15c91b.chunk.css"
+  },
+  {
+    "revision": "a622cd8bd822ed59840a",
+    "url": "/static/js/2.bdb53881.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdb53881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1ea89b9dc5358086c3",
+    "url": "/static/js/main.4a7907d5.chunk.js"
+  },
+  {
+    "revision": "3181ca61e5c5099705c8",
+    "url": "/static/js/runtime-main.77d79b37.js"
+  },
+  {
+    "revision": "4647c118d4010be60fcd2c07d1c4019f",
+    "url": "/static/media/build001.4647c118.png"
+  },
+  {
+    "revision": "9f8d112a369bbf1037d6d858fa526895",
+    "url": "/static/media/changeBox.9f8d112a.mp3"
+  },
+  {
+    "revision": "dc0e7b5a3fa63f5a38bc3e4ee9bc36e8",
+    "url": "/static/media/character.dc0e7b5a.png"
+  },
+  {
+    "revision": "88c1192193df61e300f49dfc16e69b99",
+    "url": "/static/media/game_bg.88c11921.png"
+  },
+  {
+    "revision": "269ab8f49b8bfe8cf2eb1b2be08f2985",
+    "url": "/static/media/getBox.269ab8f4.mp3"
+  },
+  {
+    "revision": "ecf9eb803668f560384f466e9703d3d8",
+    "url": "/static/media/home-bj2.ecf9eb80.png"
+  },
+  {
+    "revision": "86638e50b81ed7f393b93b8eb5ad9cac",
+    "url": "/static/media/home_bg.86638e50.png"
+  },
+  {
+    "revision": "a69999c5c769c54cb21e560a3a06ac30",
+    "url": "/static/media/selectWord.a69999c5.mp3"
+  }
+]);
